f3ff3efc727c9207e16a469d43f3bd259abce294
tranmanhhungdn1201 <tranmanh.hungdn1201@gmail.com>
2020-05-30 22:40:47 +0700

    Create todo list react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>Go to market</li>
+        <li>Buy food</li>
+        <li>Make dinner</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
